ff82febbb4f01af7938f7c5d206b9249c5d5de47
Xavier <Xavier.e.santamaria.ramirez@gmail.com>
2024-06-29 10:20:35 -0500

    Se agrego la alerta

--- assets/js/sweetAlert.js
+++ assets/js/sweetAlert.js
@@ -1,53 +1,65 @@
-Swal.fire({
-  title: "Gracias por registrarte",
-  // text:
-  // html:
-  icon: "success",
-  confirmButtonText: "Confirmar",
-  // footer:
-  // width:
-  // padding:
-  // background:
-  // grow:
-  // backdrop:
-  // timer:
-  // timerProgressBar:
-  // toast:
-  // position:
-  // allowOutsideClick:
-  // allowEscapeKey:
-  // allowEnterKey:
-  // stopKeydownPropagation:
-  // input:
-  // inputPlaceholder:
-  // inputValue:
-  // inputOptions:
-  //  customClass:
-  // 	container:
-  // 	popup:
-  // 	header:
-  // 	title:
-  // 	closeButton:
-  // 	icon:
-  // 	image:
-  // 	content:
-  // 	input:
-  // 	actions:
-  // 	confirmButton:
-  // 	cancelButton:
-  // 	footer:
-  // showConfirmButton:
-  // confirmButtonColor:
-  // confirmButtonAriaLabel:
-  // showCancelButton:
-  // cancelButtonText:
-  // cancelButtonColor:
-  // cancelButtonAriaLabel:
-  // buttonsStyling:
-  // showCloseButton:
-  // closeButtonAriaLabel:
-  // imageUrl:
-  // imageWidth:
-  // imageHeight:
-  // imageAlt:s
-});
+document
+  .getElementById("miFormulario")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    if (formularioCompleto()) {
+      Swal.fire({
+        title: "Gracias por formar parte de la familia ICON",
+        icon: "success",
+        confirmButtonText: "Confirmar",
+        showClass: {
+          popup: `
+                animate__animated
+                animate__fadeInUp
+                animate__faster
+              `,
+        },
+        hideClass: {
+          popup: `
+                animate__animated
+                animate__fadeOutDown
+                animate__faster
+              `,
+        },
+      });
+    } else {
+      alert("Por favor, complete todos los campos");
+    }
+  });
+
+function formularioCompleto() {
+  const form = document.getElementById("miFormulario");
+  const inputs = form.querySelectorAll(
+    "input[required], select[required], textarea[required]"
+  );
+
+  for (let input of inputs) {
+    if (!input.value) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// var btn = document.getElementById("btn-pop-up");
+// btn.onclick = function () {
+//   Swal.fire({
+//     title: "Gracias por registrarte",
+//     icon: "success",
+//     confirmButtonText: "Confirmar",
+//     showClass: {
+//       popup: `
+//             animate__animated
+//             animate__fadeInUp
+//             animate__faster
+//           `,
+//     },
+//     hideClass: {
+//       popup: `
+//             animate__animated
+//             animate__fadeOutDown
+//             animate__faster
+//           `,
+//     },
+//   });
+// };
